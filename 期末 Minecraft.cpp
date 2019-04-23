1d62f11a470ab5cb82a6b4b9dd06c43756bb47cc
Westwood2723 <48615707+Westwood2723@users.noreply.github.com>
2019-04-23 19:24:05 +0800

    Update 期末 Minecraft.cpp

--- 期末 Minecraft.cpp
+++ 期末 Minecraft.cpp
@@ -9,11 +9,11 @@ int main() {
 
 		int area = n * 4 + 2; 
 		for (int i = n / 2; i > 1; --i) {
-			if (n % i == 0) { //除开第一层
+			if (n % i == 0) { 
 				int j = n / i; 
-				if ((i * j + i + j) * 2 < area) { area= (i * j + i + j) * 2; } //除开第二层之前先替换
+				if ((i * j + i + j) * 2 < area) { area= (i * j + i + j) * 2; } 
 				for (int k = j / 2; k > 1; --k) {
-					if (j % k == 0) { //除开第二层
+					if (j % k == 0) { 
 						int jj = j / k;
 						if ((i * jj + jj * k + i * k) * 2 < area) { area= (i * jj + jj * k + i * k) * 2; }
 					}
